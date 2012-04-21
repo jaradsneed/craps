cdf61373f18ce9128111d02c661584dd442533d9
Jarad Sneed <jarad.sneed@gmail.com>
2012-04-20 21:20:51 -0500

    Moved Directories

---
+++ craps/Form1.Designer.vb
@@ -0,0 +1,80 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.imgDie1 = New System.Windows.Forms.PictureBox()
+        Me.imgDie2 = New System.Windows.Forms.PictureBox()
+        Me.btnRoll = New System.Windows.Forms.Button()
+        CType(Me.imgDie1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.imgDie2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'imgDie1
+        '
+        Me.imgDie1.Image = Global.craps.My.Resources.Resources.die1
+        Me.imgDie1.Location = New System.Drawing.Point(855, 12)
+        Me.imgDie1.Name = "imgDie1"
+        Me.imgDie1.Size = New System.Drawing.Size(60, 60)
+        Me.imgDie1.TabIndex = 0
+        Me.imgDie1.TabStop = False
+        '
+        'imgDie2
+        '
+        Me.imgDie2.Image = Global.craps.My.Resources.Resources.die1
+        Me.imgDie2.Location = New System.Drawing.Point(940, 12)
+        Me.imgDie2.Name = "imgDie2"
+        Me.imgDie2.Size = New System.Drawing.Size(60, 60)
+        Me.imgDie2.TabIndex = 1
+        Me.imgDie2.TabStop = False
+        '
+        'btnRoll
+        '
+        Me.btnRoll.Location = New System.Drawing.Point(1024, 32)
+        Me.btnRoll.Name = "btnRoll"
+        Me.btnRoll.Size = New System.Drawing.Size(75, 23)
+        Me.btnRoll.TabIndex = 2
+        Me.btnRoll.Text = "Roll"
+        Me.btnRoll.UseVisualStyleBackColor = True
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = Global.craps.My.Resources.Resources.crapsBG
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(1175, 649)
+        Me.Controls.Add(Me.btnRoll)
+        Me.Controls.Add(Me.imgDie2)
+        Me.Controls.Add(Me.imgDie1)
+        Me.Name = "frmMain"
+        Me.Text = "Craps"
+        CType(Me.imgDie1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.imgDie2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents imgDie1 As System.Windows.Forms.PictureBox
+    Friend WithEvents imgDie2 As System.Windows.Forms.PictureBox
+    Friend WithEvents btnRoll As System.Windows.Forms.Button
+
+End Class
---
+++ craps/Form1.vb
@@ -0,0 +1,52 @@
+﻿Public Class frmMain
+
+    Private Sub frmMain_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub btnRoll_Click(sender As System.Object, e As System.EventArgs) Handles btnRoll.Click
+        Dim low As Integer = 1
+        Dim high As Integer = 6
+        Dim die1 As Integer
+        Dim die2 As Integer
+        Dim total As Integer
+
+        die1 = Math.Floor((high - low + 1) * Rnd() + low)
+        die2 = Math.Floor((high - low + 1) * Rnd() + low)
+        total = die1 + die2
+
+        Select Case die1
+            Case 1
+                imgDie1.Image = My.Resources.die1
+            Case 2
+                imgDie1.Image = My.Resources.die2
+            Case 3
+                imgDie1.Image = My.Resources.die3
+            Case 4
+                imgDie1.Image = My.Resources.die4
+            Case 5
+                imgDie1.Image = My.Resources.die5
+            Case Else
+                imgDie1.Image = My.Resources.die6
+        End Select
+
+        Select Case die2
+            Case 1
+                imgDie2.Image = My.Resources.die1
+            Case 2
+                imgDie2.Image = My.Resources.die2
+            Case 3
+                imgDie2.Image = My.Resources.die3
+            Case 4
+                imgDie2.Image = My.Resources.die4
+            Case 5
+                imgDie2.Image = My.Resources.die5
+            Case Else
+                imgDie2.Image = My.Resources.die6
+        End Select
+
+    End Sub
+
+
+
+End Class
